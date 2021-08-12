de133ac3f55e781b76f80b55d722698c359dfe52
SavvyPlusPlus <info@savvydc.com>
2021-08-11 20:37:08 -0400

    BUILD AND APIs

---
+++ index.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const cors = require("express")
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/*", (_, res)=>{
+    res.json({
+        data: "The API is working!"
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
+
+console.log("It's alive")
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USER)
+console.log(process.env.PORT)
+console.log(process.env.GREET)
+console.log(process.env.FOOD)
+console.log(process.env.REALLY)
